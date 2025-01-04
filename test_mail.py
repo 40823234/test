15e9f749b84a4962c4da88aa298f4cc6a6345415
you <you@ews.com>
2025-01-04 13:21:02 +0800

    email with file

--- test_mail.py
+++ test_mail.py
@@ -2,6 +2,8 @@ import smtplib,ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
+from email.mime.base import MIMEBase
+from email import encoders
 
 sender = "40823234@gm.nfu.edu.tw"
 receiver = ["snipery730@gmail.com"]
@@ -16,6 +18,16 @@ for i in receiver:
 
     msg_test=MIMEText(body)
     msg.attach(msg_test)
+
+    text_path = "test.txt"
+    with open(text_path,"rb") as f:
+        part=MIMEBase("application","octet-stream")
+        part.set_payload(f.read())
+        encoders.encode_base64(part)
+        part.add_header('content-Disposition','attachment;filename="'+text_path+'"')
+        #'attachment;filename="test.txt"'   字串中間加入變數'+變數名稱+'
+        msg.attach(part)
+
     c = ssl.create_default_context()
 
     with smtplib.SMTP_SSL("smtp.gmail.com",465,context=c) as server:
